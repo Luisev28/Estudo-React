3cd9b1f0325f5938aedaba18ad4ee82f93853270
unknown <luisev28@gmail.com>
2023-01-09 23:46:49 -0300

    Criação de components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = 'Matheus';
-
-  const newName = name.toUpperCase()
-
-  function sum (a, b){
-    return a + b
-  }
-
-  const url = "https:via.placeholder.com/150"
-
   return (
     <div className="App">
-      <h2>Alterando o JSX </h2>
-      <p>Olá meu nome é {name}</p>
-      <p>Olá meu nome é {newName}</p>
-      <p>Soma {2 + 2 }</p>
-      <p>Soma {sum(1, 2)}</p>
-      <img src={url} alt="Minha Imagem"/>
-     </div>
+      <SayMyName nome="Luis" />
+
+      <Pessoa nome="Rodrigo"
+      idade="28"
+      profissao="programador"
+      foto="http://via.placeholder.com/150" />
+    </div>
   );
 }
 
---
+++ src/components/SayMyName.js
@@ -0,0 +1,14 @@
+function SayMyName(props) {
+
+    return (
+        <div>
+            <p>Fala aí {props.nome}, suave ?</p>
+            <p>Fala aí {props.idade}, suave ?</p>
+            <p>Fala aí {props.dados}, suave ?</p>
+
+
+        </div>
+    )
+}
+
+export default SayMyName
