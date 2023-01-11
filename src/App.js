a91c61f708b64944e11379d8150590adffb01d03
unknown <luisev28@gmail.com>
2023-01-10 23:45:31 -0300

    css fragments e props avançadas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
   return (
     <div className="App">
       <SayMyName nome="Luis" />
-
+      <Frase/>
+      <Frase/>
       <Pessoa nome="Rodrigo"
       idade="28"
       profissao="programador"
       foto="http://via.placeholder.com/150" />
+
+
+      <List/>
+
+
+
     </div>
   );
 }
